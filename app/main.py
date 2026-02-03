f5483c8cd4d985c0482cb6972814f4e5e797f4af
Isaque Costa <isaquecosta131105@gmail.com>
2026-02-02 21:38:04 -0300

    subindo trabalho

--- app/main.py
+++ app/main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/sum")
-def sum_numbers(a: int, b: int):
-    return {"result": a + b}
+def somar_numeros(a: int, b: int):
+    return {"resultado": a + b}
